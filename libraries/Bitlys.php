b595de1bbfdccaa4d2ef4d2d1d73a67349096b68
Aris Haryanto <arisharyanto32@gmail.com>
2016-05-17 20:19:24 +0700

    Add new function

--- libraries/Bitlys.php
+++ libraries/Bitlys.php
@@ -12,14 +12,29 @@ class Bitlys {
         $this->config = (object) $config;
     }
 
-    function sendResponse($url, $param){
-		$ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        $server_output = curl_exec($ch);
+    function sendResponse($type, $url, $param){
+    	$server_output = '';
+
+    	if($type == 'POST'){
+			$ch = curl_init();
+	        curl_setopt($ch, CURLOPT_URL, $url);
+	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	        curl_setopt($ch, CURLOPT_POST, count($param));
+	        curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+	        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	        $server_output = curl_exec($ch);
+	        curl_close($ch);
+
+	    }else if($type == 'GET'){
+	    	$ch = curl_init();
+	        curl_setopt($ch, CURLOPT_URL, $url.'?'.$param);
+	    	curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+	    	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	    	$server_output = curl_exec($ch);
+	        curl_close($ch);
+
+	    }
+
         return json_decode($server_output);
         
 	}
@@ -31,7 +46,7 @@ class Bitlys {
                         'longUrl'  => $url
                       	);
 
-	    $result = $this->sendResponse($host, $param);
+	    $result = $this->sendResponse('POST', $host, $param);
 	    return $result->data->url;
 	}
 
@@ -43,8 +58,26 @@ class Bitlys {
 						'shortUrl' => $url
 						);
 
-	    $result = $this->sendResponse($host, $param);
+	    $result = $this->sendResponse('POST', $host, $param);
 	    return $result->data->expand[0]->long_url;
+	}
+
+	function clicks($url){
+		$host 	= 'https://api-ssl.bitly.com/v3/link/clicks';
+		$param 	= 'access_token='.$this->config->access_token.
+                  	'&link='.$url;
+
+	    $result = $this->sendResponse('GET', $host, $param);
+	    return $result->data->link_clicks;
+	}
+
+	function countries($url){
+		$host 	= 'https://api-ssl.bitly.com/v3/link/countries';
+		$param 	= 'access_token='.$this->config->access_token.
+                  	'&link='.$url;
+
+	    $result = $this->sendResponse('GET', $host, $param);
+	    return $result->data->countries;
 	}
 
 }
